c58a1caa3a30bb9b7560287560b27de1dccededa
Ru Tan <jutan2012@gmail.com>
2017-01-11 14:28:03 +0100

    Exer 5 DONE. Ajout <form methode = "GET" action = "")

---
+++ Exer5/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Formulaire5</title>
+ </head>
+ <body>
+
+ 	<form method="GET" action="">
+ 	<select name="genre">
+    <option>Mr</option>
+    <option>Mme</option>
+    </select>
+ 		<label for="nom">Nom: </label>
+ 		<input type="text" name="nom">
+ 		<label for="prenom">Prénom: </label>
+ 		<input type="text" name="prenom">
+ 		<input type="submit" value="Envoyer">
+ 	</form>
+ </body>
+ </html>
